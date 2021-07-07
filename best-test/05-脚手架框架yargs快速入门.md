d451df8c9d0ad5a6249c8293ed77982b21e0ecda
罗智毅 <zhiyiluo@drgou.com>
2021-07-07 14:50:21 +0800

    脚手架框架yargs快速入门

--- best-test/05-脚手架框架yargs快速入门.md
+++ best-test/05-脚手架框架yargs快速入门.md
@@ -27,7 +27,7 @@ const cli = yargs(arg);
 cli
   .usage('Usage: best-test [commoand] <options>') // 用法
   .demandCommand(1, 'A command is required. Pass --help to see all available commands and options.') // 至少输入一个命令，不然会提示
-  .recommendCommands()
+  .recommendCommands() // 自动命令提醒
   .alias('h', 'help') // 别名
   .alias('v', 'version')
   .wrap(cli.terminalWidth()) // 修改宽度
@@ -51,3 +51,60 @@ cli
   .group(['debug'], 'Dev Options') // 将命令分组
   .strict().argv;
 ```
+
+### command
+
+```javascript
+.command(
+  'init [name]',
+  'Do init a project',
+  (yargs) => {
+    yargs.option('name', {
+      type: 'string',
+      describe: 'Name of a project',
+      alias: 'n',
+    });
+  },
+  (argv) => {
+    console.log(argv);
+  },
+)
+.command({
+  command: 'list',
+  aliases: ['ls', 'la', 'll'],
+  describe: 'List local packages',
+  builder: (yargs) => {
+    console.log(yargs);
+  },
+  handler: (argv) => {
+    console.log(argv);
+  },
+})
+```
+
+### fail
+
+```javascript
+.fail((err, msg) => {
+  console.log('err:', err);
+  console.log('msg:', msg);
+})
+```
+
+### parse
+
+将两个参数合并
+
+```javascript
+const argv = process.argv.slice(2);
+
+const context = {
+  bVersion: pkg.version,
+};
+
+.parse(argv, context);
+```
+
+### recommendCommands
+
+自动命令提醒
--- best-test/bin/index.js
+++ best-test/bin/index.js
@@ -3,14 +3,21 @@
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const dedent = require('dedent'); // 去掉前后的空格符
+const pkg = require('../package.json');
 
 const arg = hideBin(process.argv);
 const cli = yargs(arg);
+const argv = process.argv.slice(2);
+
+const context = {
+  bVersion: pkg.version,
+};
 
 cli
-  .usage('Usage: best-test [commoand] <options>') // 用法
+  .strict()
+  .usage('Usage: $0 [commoand] <options>') // 用法
   .demandCommand(1, 'A command is required. Pass --help to see all available commands and options.') // 至少输入一个命令，不然会提示
-  .recommendCommands()
+  .recommendCommands() // 自动命令提醒
   .alias('h', 'help') // 别名
   .alias('v', 'version')
   .wrap(cli.terminalWidth()) // 修改宽度
@@ -18,7 +25,10 @@ cli
     dedent`  Your own footer description  
   hi`,
   ) // 在结尾加上你想说的话 dedent`hi` 等价于 dedent(`hi`)
-
+  .fail((err, msg) => {
+    console.log('err:', err);
+    // console.log('msg:', msg);
+  })
   .options({
     debug: {
       type: 'boolean',
@@ -32,4 +42,29 @@ cli
     alias: 'r',
   }) // 定义命令
   .group(['debug'], 'Dev Options') // 将命令分组
-  .strict().argv;
+  .command(
+    'init [name]',
+    'Do init a project',
+    (yargs) => {
+      yargs.option('name', {
+        type: 'string',
+        describe: 'Name of a project',
+        alias: 'n',
+      });
+    },
+    (argv) => {
+      console.log(argv);
+    },
+  )
+  .command({
+    command: 'list',
+    aliases: ['ls', 'la', 'll'],
+    describe: 'List local packages',
+    builder: (yargs) => {
+      console.log(yargs);
+    },
+    handler: (argv) => {
+      console.log(argv);
+    },
+  })
+  .parse(argv, context);
