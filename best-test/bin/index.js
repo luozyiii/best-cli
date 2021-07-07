9740abed0a6e72dc7dcead0570abb30a9656ec1b
罗智毅 <zhiyiluo@drgou.com>
2021-07-07 15:20:49 +0800

    javascript事件循环

--- best-test/bin/index.js
+++ best-test/bin/index.js
@@ -65,6 +65,24 @@ cli
     },
     handler: (argv) => {
       console.log(argv);
+      console.log('start');
+      setTimeout(() => {
+        console.log('setTimeout');
+      }, 0);
+      new Promise(() => {
+        let chain = Promise.resolve();
+        chain.then(() => console.log('chain1'));
+        chain.then(() => console.log('chain2'));
+        chain.then(() => console.log('chain3'));
+      });
+      let chain = Promise.resolve();
+      chain.then(() => console.log('chain4'));
+      setTimeout(() => {
+        let chain = Promise.resolve();
+        chain.then(() => console.log('chain5'));
+      }, 0);
+      console.log('end');
+      // start end chain1 chain2 chain3 chain4 setTimeout chain5
     },
   })
   .parse(argv, context);
