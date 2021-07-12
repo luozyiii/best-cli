3cfb3db315c6fa63b0bade90cef14f6d7b0978a4
罗智毅 <zhiyiluo@drgou.com>
2021-07-12 17:48:00 +0800

    debug模式和对未知命令的监听

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -14,23 +14,61 @@ const semver = require('semver');
 const colors = require('colors/safe');
 const userHome = require('user-home');
 const pathExists = require('path-exists');
-
+const commander = require('commander');
 const log = require('@best-cli/log');
+
 const pkg = require('../package.json');
 const constant = require('./const');
 
+const program = new commander.Command();
 function core() {
   try {
     checkPkgVersion();
     checkNodeVersion();
     // checkRoot();
     checkUserHome();
-    checkInputArgs();
-    log.verbose('debug', 'test debug log'); // 开启debug模式后可打印这句
-    checkEnv();
+    // checkInputArgs();
+    // checkEnv();
     checkGlobalUpdate();
+    registerCommand();
   } catch (e) {
     log.error(e.message);
+  }
+}
+
+// 注册命令
+function registerCommand() {
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage('<command> [options]')
+    .version(pkg.version)
+    .option('-d, --debug', '是否开启调试模式', false);
+
+  // 开启 debug 模式
+  program.on('option:debug', function () {
+    if (program.opts().debug) {
+      process.env.LOG_LEVEL = 'verbose';
+    } else {
+      process.env.LOG_LEVEL = 'info';
+    }
+  });
+  log.level = process.env.LOG_LEVEL;
+  log.verbose('debug', 'test debug log'); // 开启debug模式后可打印这句
+
+  // 对未知命令的监听
+  program.on('command:*', function (obj) {
+    console.error('未知的命令:', obj[0]);
+    const availableCommmands = program.commands.map((cmd) => cmd.name());
+    if (availableCommmands.length > 0) {
+      console.log('可用命令:', availableCommmands.join(','));
+    }
+  });
+
+  program.parse(process.argv);
+
+  if (program.args && program.args.length < 1) {
+    program.outputHelp();
+    console.log('');
   }
 }
 
