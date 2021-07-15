07b6b56d0477374c7afe7e594073b95532b915f7
罗智毅 <zhiyiluo@drgou.com>
2021-07-15 10:15:51 +0800

    shell优化

--- shell/install.js
+++ shell/install.js
@@ -1,6 +1,6 @@
 const cp = require('child_process');
 const path = require('path');
 
-cp.execFile(path.resolve(__dirname, 'install.sh'), [], function (err, stdout, stderr) {
+cp.exec(`sh ${path.resolve(__dirname, 'install.sh')}`, [], function (err, stdout, stderr) {
   console.log(err, stdout, stderr);
 });
